2745d8360a4438ef88288d0ffb8c04b3edf536fb
X-x <37548204+killgriff22@users.noreply.github.com>
2024-12-09 00:27:33 -0500

    Update EEPROM_PROCESSOR.ino

--- EEPROM_PROCESSOR.ino
+++ EEPROM_PROCESSOR.ino
@@ -4,9 +4,9 @@
 // start reading from the first byte (address 0) of the EEPROM
 // arduino uno r3 has 1024 bytes
 int address = 0;
-int startup_buffer[10];
-int registers[256];
-int stack[256];
+byte startup_buffer[10];
+byte registers[256];
+byte stack[256];
 byte value;
 
 void setup() {
@@ -21,74 +21,95 @@ void setup() {
 
 
 void reset(){
+  //read the first 10 bytes of the program.
+  /*
+  starting Byte, little endian
+  program data
+  program data
+  programming flags (N/A * 7, Verbose programming )
+  program data (etc.)
+  */
   Serial.println();
   for (int i=0;i<10;i++){
     startup_buffer[i] = EEPROM.read(i);
   }
   address = startup_buffer[0];
 }
-
+void reset_no_print(){
+  //same as reset, without printing a new line. 
+  for (int i=0;i<10;i++){
+    startup_buffer[i] = EEPROM[i];
+  }
+  address = startup_buffer[0];
+}
+void inc_Addr(byte amt=1){
+  address=address+amt;
+  if (startup_buffer[4] & 0x02){ 
+    if (address < 0x10){Serial.print("0");}
+    Serial.print(address, 16);
+    Serial.print("\t");
+    if (value < 0x10){Serial.print("0");}
+    Serial.print(value, 16);
+    Serial.println();
+    delay(500);
+  }
+}
 void loop() {
   // read a byte from the current address of the EEPROM
-  int val;
-  int reg1;
-  int reg2;
-  int stackaddr;
-  int pointer;
+  byte val;
+  byte reg1;
+  byte reg2;
+  byte stackaddr;
+  byte pointer;
+  byte buff[1];
   if (address == EEPROM.length()) {
     return;
   }
   value = EEPROM.read(address);
-  /*if (address < 0x10){Serial.print("0");}
-  Serial.print(address, 16);
-  Serial.print("\t");
-  if (value < 0x10){Serial.print("0");}
-  Serial.print(value, 16);
-  Serial.println();*/
   switch (value){
     case 0x00:// nop
       break;
     case 0x01:// print value at next address
-      address++;
+      inc_Addr();
       Serial.write(EEPROM.read(address));
       Serial.flush();
       break;
-    case 0x02:// print value pointed to by next address
-      address++;
-      Serial.write(EEPROM.read(EEPROM.read(address)));
+    case 0x02:// print value in register
+      inc_Addr();
+      Serial.write(registers[address]);
       Serial.flush();
       break;
     case 0x03:// push value to register
-      address++;
+      inc_Addr();
       val = EEPROM.read(address);
-      address++;
+      inc_Addr();
       reg1 = EEPROM.read(address);
       registers[reg1] = val;
       break;
     case 0x04:// push register to stack address
-      address++;
+      inc_Addr();
       reg1 = EEPROM.read(address);
-      address++;
+      inc_Addr();
       stackaddr = EEPROM.read(address);
       stack[stackaddr] = registers;
       break;
     case 0x05:// push register to register
-      address++;
+      inc_Addr();
       reg1 = EEPROM.read(address);
-      address++;
+      inc_Addr();
       reg2 = EEPROM.read(address);
       registers[reg1] = registers[reg2];
       break;
     case 0x06:// pull stack address to register
-      address++;
+      inc_Addr();
       stackaddr = EEPROM.read(address);
-      address++;
+      inc_Addr();
       reg1 = EEPROM.read(address);
       registers[reg1] = stack[stackaddr];
       break;
     // jump to pointer if 00 register > 01 register
     case 0x07:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] > registers[1]){
         address = pointer;
@@ -97,7 +118,7 @@ void loop() {
       break;
     // jump to pointer if 00 register < 01 register
     case 0x08:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] < registers[1]){
         address = pointer;
@@ -106,7 +127,7 @@ void loop() {
       break;
     // jump to pointer if 00 register >= 01 register
     case 0x09:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] >= registers[1]){
         address = pointer;
@@ -115,7 +136,7 @@ void loop() {
       break;
     // jump to pointer if 00 register <= 01 register
     case 0x0A:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] <= registers[1]){
         address = pointer;
@@ -124,7 +145,7 @@ void loop() {
       break;
     // jump to pointer if 00 register == 01 register
     case 0x0B:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] == registers[1]){
         address = pointer;
@@ -133,16 +154,94 @@ void loop() {
       break;
     // jump to pointer if 00 register != 01 register
     case 0x0C:
-      address++;
+      inc_Addr();
       pointer = EEPROM.read(address);
       if (registers[0] != registers[1]){
         address = pointer;
         return;
       }
       break;
+    // push available serial bytes to register
+    case 0x0D:
+      inc_Addr();
+      reg1=EEPROM[address];
+      registers[reg1] = Serial.available();
+      break;
+    // pop first available byte to register from serial
+    case 0x0E:
+      inc_Addr();
+      reg1=EEPROM[address];
+      Serial.readBytes(buff,1);
+      registers[reg1] = buff[0];
+      break;
+    // legacy - print value at pointer
+    case 0x0F:// print value in register
+      inc_Addr();
+      Serial.write(EEPROM.read(EEPROM.read(address)));
+      Serial.flush();
+      break;
+    // jump ahead n bytes if 00 register > 01 register
+    case 0x10:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] > registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    // jump ahead n bytes if 00 register < 01 register
+    case 0x11:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] < registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    // jump ahead n bytes if 00 register >= 01 register
+    case 0x12:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] >= registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    // jump ahead n bytesr if 00 register <= 01 register
+    case 0x13:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] <= registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    // jump ahead n bytes if 00 register == 01 register
+    case 0x14:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] == registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    // jump ahead n bytes if 00 register != 01 register
+    case 0x15:
+      inc_Addr();
+      pointer = EEPROM.read(address);
+      if (registers[0] != registers[1]){
+        address += pointer;
+        return;
+      }
+      break;
+    //reset functions
     case 0xFF:
       reset();
       return;
+    case 0xFE:
+      reset_no_print();
+      return;
   }
-  address++;
+  inc_Addr();
+  Serial.println("instruction processed");
 }
